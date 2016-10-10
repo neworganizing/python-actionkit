dabdfd2d72e15e2edc150a39d22e83f37a5d139d
Scott Reynen <scott.reynen@moveon.org>
2016-10-10 16:34:17 -0600

    add event field methods and get_event()

--- actionkit/api/event.py
+++ actionkit/api/event.py
@@ -6,6 +6,12 @@ from actionkit.api.action import AKActionAPI
 
 class AKEventAPI(base.ActionKitAPI):
 
+    def get_event(self, event_id):
+        result = self.client.get(
+            '%s/rest/v1/event/%s' % (self.base_url, event_id)
+        )
+        return {'res': result}
+
     def create_signup(self, user_id, event_id, page_id, role='attendee', status='active', fields=None):
         data = {
             'event': '/rest/v1/event/%s/' % event_id,
@@ -35,5 +41,37 @@ class AKEventAPI(base.ActionKitAPI):
         result = self.client.put(
             '%s/rest/v1/eventsignup/%s/' % (self.base_url, signup_id),
             data=json.dumps(data)
+        )
+        return {'res': result}
+
+    def create_event_field(self, event_id, field_name, field_value):
+        data = {
+            'event': '/rest/v1/event/%s/' % event_id,
+            'name': field_name,
+            'value': field_value,
+        }
+        result = self.client.post(
+            '%s/rest/v1/eventfield/' % self.base_url,
+            data=json.dumps(data)
+        )
+        return {'res': result}
+
+    def get_event_fields(self, event_id=None, field_name=None, field_value=None):
+        data = {}
+        if event_id is not None:
+            data['parent'] = '/rest/v1/event/%s/' % event_id
+        if field_name is not None:
+            data['name'] = field_name
+        if field_value is not None:
+            data['value'] = field_value
+        result = self.client.get(
+            '%s/rest/v1/eventfield/' % self.base_url,
+            params=data
+        )
+        return {'res': result}
+
+    def delete_event_field(self, eventfield_id):
+        result = self.client.delete(
+            '%s/rest/v1/eventfield/%s' % (self.base_url, eventfield_id)
         )
         return {'res': result}
