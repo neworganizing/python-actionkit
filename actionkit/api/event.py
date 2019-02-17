aa1a3b91b625047fbaf3a6db3deb867d6032f92b
Scott Reynen <scott.reynen@moveon.org>
2019-02-17 14:35:38 -0700

    Update list_signups for Python 3

--- actionkit/api/event.py
+++ actionkit/api/event.py
@@ -53,13 +53,18 @@ class AKEventAPI(base.ActionKitAPI):
             rv['fieldresults'] = fieldresults
         return rv
 
-    def list_signups(self, user_id, query_params={}):
+    def list_signups(self, user_id=False, query_params={}):
         def idfromurl(path):
             if '/' in path:
                 return re.findall(r'/(\d+)/?$', path)[0]
-        result = self.client.get(
-            '%s/rest/v1/eventsignup/?user=%s&%s' % (
-                self.base_url, user_id, urllib.urlencode(query_params)))
+        if user_id:
+            result = self.client.get(
+                '%s/rest/v1/eventsignup/?user=%s&%s' % (
+                    self.base_url, user_id, urllib.parse.urlencode(query_params)))
+        else:
+            result = self.client.get(
+                '%s/rest/v1/eventsignup/?%s' % (
+                    self.base_url, urllib.parse.urlencode(query_params)))
         final_result = {'res': result}
         if result.status_code == 200:
             json = result.json()
